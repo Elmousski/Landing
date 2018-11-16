e45a3249c49bc59be508af3e3ca2caf89c649251
Houivet Charlotte <charlotte.houivet@outlook.com>
2018-11-16 03:51:05 +0100

    responsive email form

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   get '/', to: 'static#index'
-  get 'geek', to: 'static#geek'
+  get 'tech', to: 'static#tech'
   get 'maubeuge', to: 'static#maubeuge'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
